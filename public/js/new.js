0d904565fe0f988675a7c96e648c3c68c64af53f
Ali Jalilvand <ali.jalilvand2203@gmail.com>
2025-03-15 17:23:27 +0330

    Show Sub Categories and Sub Sub Categories of Each Category & Handle Back to Previous Level

--- public/js/new.js
+++ public/js/new.js
@@ -1,5 +1,5 @@
-import { getAllCategories } from "../../utils/shared.js"
-import { isLogin } from "../../utils/utils.js"
+import { baseUrl } from "../../utils/shared.js"
+import { addParamToURL, getParamFromURL, isLogin } from "../../utils/utils.js"
 
 window.addEventListener('load', async () => {
     const loadingContainer = document.querySelector('#loading-container')
@@ -7,36 +7,31 @@ window.addEventListener('load', async () => {
     const categoriesSection = document.querySelector('#categories')
     const descriptionCheckbox = document.querySelector('#description-checkbox')
 
-    console.log(categoriesContainer);
-
 
     const isUserLogin = await isLogin()
     if (!isUserLogin) {
         return location.href = '/pages/posts.html'
     }
-
-    // Website Loader
     loadingContainer.style.display = 'none'
-
-    getAllCategories().then(categories => {
-        showCategories(categories)
-
-        descriptionCheckbox?.addEventListener('change', () => {
-            showCategories(categories)
-        })
-    })
-
-
 
 
     //! Functions
 
-    const showCategories = (categories) => {
+    const generateCategoriesTemplate = (categories, title, id) => {
         categoriesSection.innerHTML = ''
+
+        if (title) {
+            categoriesSection.insertAdjacentHTML('beforeend', `
+                <div class="back" onclick="${id ? `categoryClickHandler('${id}')` : `backToAllCategories()`}">
+                     <i class="bi bi-arrow-right"></i>
+                    <p>بازگشت به ${title}</p>
+                </div>
+            `)
+        }
 
         categories.forEach(category => {
             categoriesSection.insertAdjacentHTML('beforeend', `
-                <div class="box">
+                <div class="box" onclick="categoryClickHandler('${category._id}')">
                     <div class="details">
                         <div>
                           <i class="bi bi-house-door"></i>
@@ -49,6 +44,46 @@ window.addEventListener('load', async () => {
                 `)
         })
     }
+
+
+    window.categoryClickHandler = (categoryID) => {
+        const category = categories.find(category => category._id === categoryID)
+
+        if (category) {
+            //* Main Category
+            generateCategoriesTemplate(category.subCategories, 'همه دسته ها', null)
+        } else {
+            const allSubCategories = categories.flatMap(category => category.subCategories)
+            const subCategory = allSubCategories.find(subCategory => subCategory._id === categoryID)
+
+            if (subCategory) {
+                //* Sub Category
+                const subCategoryParent = categories.find(category => category._id === subCategory.parent)
+                generateCategoriesTemplate(subCategory.subCategories, subCategoryParent.title, subCategoryParent._id)
+            } else {
+                //* Sub Sub Category
+                location.href = `/pages/new/registerPost.html?subCategoryID=${categoryID}`
+            }
+        }
+
+    }
+
+
+    window.backToAllCategories = () => {
+        generateCategoriesTemplate(categories)
+    }
+
+
+    // ===========================================================================================================================
+
+    const res = await fetch(`${baseUrl}/v1/category`)
+    const response = await res.json()
+    const categories = response.data.categories
+
+    generateCategoriesTemplate(categories)
+    descriptionCheckbox?.addEventListener('change', () => {
+        generateCategoriesTemplate(categories)
+    })
 
 
 })
